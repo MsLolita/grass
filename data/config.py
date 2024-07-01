96eb923f034eb4969b45191e5ea9c06e32cc77ce
SIARHEI PALISHCHUK <smeshny@gmail.com>
2024-07-01 13:35:03 +0200

    Sync config.py with master branch

--- data/config.py
+++ data/config.py
@@ -22,7 +22,7 @@ IMAP_DOMAIN = ""  # not always works
 
 CLAIM_REWARDS_ONLY = False  # claim tiers rewards only (https://app.getgrass.io/dashboard/referral-program)
 
-STOP_ACCOUNTS_WHEN_SITE_IS_DOWN = False  # stop account for 20 minutes, to reduce proxy traffic usage
+STOP_ACCOUNTS_WHEN_SITE_IS_DOWN = True  # stop account for 20 minutes, to reduce proxy traffic usage
 CHECK_POINTS = False  # show point for each account every nearly 10 minutes
 SHOW_LOGS_RARELY = False  # not always show info about actions to decrease pc influence
 
