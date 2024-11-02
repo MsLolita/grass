0e09ac8abf57e327bbf4b19956496fffe41a27c8
Mslolita <oblikovuizapuc@gmail.com>
2024-11-02 14:13:33 +0200

    change by default to 5 threads

--- data/config.py
+++ data/config.py
@@ -1,4 +1,4 @@
-THREADS = 1  # for register account / claim rewards mode / approve email mode
+THREADS = 5  # for register account / claim rewards mode / approve email mode
 MIN_PROXY_SCORE = 50  # for mining mode
 
 #########################################
