6d595b2fe0bed0991d408f21537e0c7239ebc4aa
Mslolita <oblikovuizapuc@gmail.com>
2024-11-26 21:12:54 +0200

    handle connection error

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import random
 import sys
 import traceback
 
+import aiohttp
 from art import text2art
 from imap_tools import MailboxLoginError
 from termcolor import colored, cprint
@@ -109,6 +110,8 @@ async def worker_task(_id, account: str, proxy: str = None, wallet: str = None, 
     #     logger.warning(e)
     except EmailApproveLinkNotFoundException as e:
         logger.warning(e)
+    except aiohttp.ClientError as e:
+        logger.warning(f"{_id} | Some connection error: {e}...")
     except Exception as e:
         logger.error(f"{_id} | not handled exception | error: {e} {traceback.format_exc()}")
     finally:
